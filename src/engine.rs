68abe8a9efff4f97118f75b8ea5f721f4512c6b1
PatrickBarreira0 <patrick.barreira@cittamobi.com.br>
2026-02-28 20:04:47 -0300

    feat: completely refactored engine to match each effect of the moves i.e damage status etc

--- src/engine.rs
+++ src/engine.rs
@@ -1,4 +1,6 @@
 use crate::pokemon::{Pokemon, PokemonType};
+use crate::moves::MoveEffect;
+use crate::types::Status;
 
 pub struct BattleEngine {
     pub player: Pokemon,
@@ -14,10 +16,9 @@ impl BattleEngine {
         match (attacker_type, defender_type) {
             (PokemonType::Fire, PokemonType::Grass) => 2.0,
             (PokemonType::Grass, PokemonType::Fire) => 0.5,
-
             (PokemonType::Water, PokemonType::Fire) => 2.0,
             (PokemonType::Fire, PokemonType::Water) => 0.5,
-            _=> 1.0, // for each case not listed, the multiplier is 1.0
+            _ => 1.0, // for each case not listed, the multiplier is 1.0
         }
     }
 
@@ -26,27 +27,49 @@ impl BattleEngine {
             return;
         }
 
-        let used_move = &attacker.moves[move_index];
-
-        let type_multiplier = Self::get_type_multiplier(&used_move.move_type, &defender.primary_type);
-        
-        let stab_multiplier = if used_move.move_type == attacker.primary_type {
-            1.5
-        } else {
-            1.0
-        };
-       
-        let damage = (used_move.power as f32 * type_multiplier * stab_multiplier) as u32;
-
+        // clone the move so we don't hold a borrow on attacker while we also need to mutate defender
+        let used_move = attacker.moves[move_index].clone();
         println!("\n> {} used {}!", attacker.name, used_move.name);
 
-        if type_multiplier > 1.0 { println!("It's super effective!"); }
-        else if type_multiplier < 1.0 { println!("It's not very effective..."); }
+        for effect in &used_move.effects { //loop over effects
+            match effect {
+                MoveEffect::Damage => {
+                    let type_multiplier = Self::get_type_multiplier(&used_move.move_type, &defender.primary_type);
+                    let stab_multiplier = if used_move.move_type == attacker.primary_type {
+                        1.5
+                    } else {
+                        1.0
+                    };
+                    let damage = (used_move.power as f32 * type_multiplier * stab_multiplier) as u32;
 
-        if stab_multiplier > 1.0 { println!("*STAB bonus applied!*"); }
+                    if type_multiplier > 1.0 { println!("It's super effective!"); }
+                    else if type_multiplier < 1.0 { println!("It's not very effective..."); }
+                    if stab_multiplier > 1.0 { println!("*STAB bonus applied!*"); }
 
-        defender.take_damage(damage);
-        println!("> {} took {} damage!", defender.name, damage);
+                    defender.take_damage(damage);
+                    println!("> {} took {} damage!", defender.name, damage);
+                }
 
+                MoveEffect::ApplyStatus { status, chance } => {
+                    let roll: f32 = rand::random();
+
+                    if roll < *chance {
+                        if defender.status.is_none() {
+                            println!("> {} was afflicted with {:?}!", defender.name, status);
+                            defender.status = Some(status.clone());
+                        } else {
+                            println!("> But it failed — {} already has a status condition!", defender.name);
+                        }
+                    }
+                }
+
+                MoveEffect::ModifyStat { .. } => {
+                    println!("> Stat modification not yet implemented.");
+                }
+                MoveEffect::Heal { .. } => {
+                    println!("> Healing not yet implemented.");
+                }
+            }
+        }
     }
 }
