26d04080ee12bbdac4e13dca798cde51aed1cd1a
PatrickBarreira0 <patrick.barreira@cittamobi.com.br>
2026-03-01 17:40:12 -0300

    refactor: move get type multiplier logic

--- src/battle/engine.rs
+++ src/battle/engine.rs
@@ -1,6 +1,6 @@
 use crate::pokemon::{Pokemon, PokemonType};
-use crate::moves::{MoveEffect, StatTarget};
-use crate::types::Status;
+use crate::moves::{MoveEffect, Stat, StatTarget};
+use crate::types::{Status, get_type_multiplier};
 
 // handles all damage calculation, move execution, and status effect processing for individual pokemon actions
 pub struct BattleEngine {
@@ -30,150 +30,6 @@ impl BattleEngine {
              6 => 4.00,
 
             _ => 1.00,
-        }
-    }
-
-    pub fn get_type_multiplier(attacker_type: &PokemonType, defender_type: &PokemonType) -> f32 {
-        match (attacker_type, defender_type) {
-            (PokemonType::Normal, PokemonType::Rock) => 0.5,
-            (PokemonType::Normal, PokemonType::Steel) => 0.5,
-            (PokemonType::Normal, PokemonType::Ghost) => 0.0,
-
-            (PokemonType::Fire, PokemonType::Grass) => 2.0,
-            (PokemonType::Fire, PokemonType::Ice) => 2.0,
-            (PokemonType::Fire, PokemonType::Bug) => 2.0,
-            (PokemonType::Fire, PokemonType::Steel) => 2.0,
-            (PokemonType::Fire, PokemonType::Fire) => 0.5,
-            (PokemonType::Fire, PokemonType::Water) => 0.5,
-            (PokemonType::Fire, PokemonType::Rock) => 0.5,
-            (PokemonType::Fire, PokemonType::Dragon) => 0.5,
-
-            (PokemonType::Water, PokemonType::Fire) => 2.0,
-            (PokemonType::Water, PokemonType::Ground) => 2.0,
-            (PokemonType::Water, PokemonType::Rock) => 2.0,
-            (PokemonType::Water, PokemonType::Water) => 0.5,
-            (PokemonType::Water, PokemonType::Grass) => 0.5,
-            (PokemonType::Water, PokemonType::Dragon) => 0.5,
-
-            (PokemonType::Electric, PokemonType::Water) => 2.0,
-            (PokemonType::Electric, PokemonType::Flying) => 2.0,
-            (PokemonType::Electric, PokemonType::Electric) => 0.5,
-            (PokemonType::Electric, PokemonType::Grass) => 0.5,
-            (PokemonType::Electric, PokemonType::Dragon) => 0.5,
-            (PokemonType::Electric, PokemonType::Ground) => 0.0,
-
-            (PokemonType::Grass, PokemonType::Water) => 2.0,
-            (PokemonType::Grass, PokemonType::Ground) => 2.0,
-            (PokemonType::Grass, PokemonType::Rock) => 2.0,
-            (PokemonType::Grass, PokemonType::Fire) => 0.5,
-            (PokemonType::Grass, PokemonType::Grass) => 0.5,
-            (PokemonType::Grass, PokemonType::Poison) => 0.5,
-            (PokemonType::Grass, PokemonType::Flying) => 0.5,
-            (PokemonType::Grass, PokemonType::Bug) => 0.5,
-            (PokemonType::Grass, PokemonType::Dragon) => 0.5,
-            (PokemonType::Grass, PokemonType::Steel) => 0.5,
-
-            (PokemonType::Ice, PokemonType::Grass) => 2.0,
-            (PokemonType::Ice, PokemonType::Ground) => 2.0,
-            (PokemonType::Ice, PokemonType::Flying) => 2.0,
-            (PokemonType::Ice, PokemonType::Dragon) => 2.0,
-            (PokemonType::Ice, PokemonType::Fire) => 0.5,
-            (PokemonType::Ice, PokemonType::Water) => 0.5,
-            (PokemonType::Ice, PokemonType::Ice) => 0.5,
-            (PokemonType::Ice, PokemonType::Steel) => 0.5,
-
-            (PokemonType::Fighting, PokemonType::Normal) => 2.0,
-            (PokemonType::Fighting, PokemonType::Ice) => 2.0,
-            (PokemonType::Fighting, PokemonType::Rock) => 2.0,
-            (PokemonType::Fighting, PokemonType::Dark) => 2.0,
-            (PokemonType::Fighting, PokemonType::Steel) => 2.0,
-            (PokemonType::Fighting, PokemonType::Poison) => 0.5,
-            (PokemonType::Fighting, PokemonType::Flying) => 0.5,
-            (PokemonType::Fighting, PokemonType::Psychic) => 0.5,
-            (PokemonType::Fighting, PokemonType::Bug) => 0.5,
-            (PokemonType::Fighting, PokemonType::Fairy) => 0.5,
-            (PokemonType::Fighting, PokemonType::Ghost) => 0.0,
-
-            (PokemonType::Poison, PokemonType::Grass) => 2.0,
-            (PokemonType::Poison, PokemonType::Fairy) => 2.0,
-            (PokemonType::Poison, PokemonType::Poison) => 0.5,
-            (PokemonType::Poison, PokemonType::Ground) => 0.5,
-            (PokemonType::Poison, PokemonType::Rock) => 0.5,
-            (PokemonType::Poison, PokemonType::Ghost) => 0.5,
-            (PokemonType::Poison, PokemonType::Steel) => 0.0,
-
-            (PokemonType::Ground, PokemonType::Fire) => 2.0,
-            (PokemonType::Ground, PokemonType::Electric) => 2.0,
-            (PokemonType::Ground, PokemonType::Poison) => 2.0,
-            (PokemonType::Ground, PokemonType::Rock) => 2.0,
-            (PokemonType::Ground, PokemonType::Steel) => 2.0,
-            (PokemonType::Ground, PokemonType::Grass) => 0.5,
-            (PokemonType::Ground, PokemonType::Bug) => 0.5,
-            (PokemonType::Ground, PokemonType::Flying) => 0.0,
-
-            (PokemonType::Flying, PokemonType::Grass) => 2.0,
-            (PokemonType::Flying, PokemonType::Fighting) => 2.0,
-            (PokemonType::Flying, PokemonType::Bug) => 2.0,
-            (PokemonType::Flying, PokemonType::Electric) => 0.5,
-            (PokemonType::Flying, PokemonType::Rock) => 0.5,
-            (PokemonType::Flying, PokemonType::Steel) => 0.5,
-
-            (PokemonType::Psychic, PokemonType::Fighting) => 2.0,
-            (PokemonType::Psychic, PokemonType::Poison) => 2.0,
-            (PokemonType::Psychic, PokemonType::Psychic) => 0.5,
-            (PokemonType::Psychic, PokemonType::Steel) => 0.5,
-            (PokemonType::Psychic, PokemonType::Dark) => 0.0,
-
-            (PokemonType::Bug, PokemonType::Grass) => 2.0,
-            (PokemonType::Bug, PokemonType::Psychic) => 2.0,
-            (PokemonType::Bug, PokemonType::Dark) => 2.0,
-            (PokemonType::Bug, PokemonType::Fire) => 0.5,
-            (PokemonType::Bug, PokemonType::Fighting) => 0.5,
-            (PokemonType::Bug, PokemonType::Poison) => 0.5,
-            (PokemonType::Bug, PokemonType::Flying) => 0.5,
-            (PokemonType::Bug, PokemonType::Ghost) => 0.5,
-            (PokemonType::Bug, PokemonType::Steel) => 0.5,
-            (PokemonType::Bug, PokemonType::Fairy) => 0.5,
-
-            (PokemonType::Rock, PokemonType::Fire) => 2.0,
-            (PokemonType::Rock, PokemonType::Ice) => 2.0,
-            (PokemonType::Rock, PokemonType::Flying) => 2.0,
-            (PokemonType::Rock, PokemonType::Bug) => 2.0,
-            (PokemonType::Rock, PokemonType::Fighting) => 0.5,
-            (PokemonType::Rock, PokemonType::Ground) => 0.5,
-            (PokemonType::Rock, PokemonType::Steel) => 0.5,
-
-            (PokemonType::Ghost, PokemonType::Psychic) => 2.0,
-            (PokemonType::Ghost, PokemonType::Ghost) => 2.0,
-            (PokemonType::Ghost, PokemonType::Dark) => 0.5,
-            (PokemonType::Ghost, PokemonType::Normal) => 0.0,
-
-            (PokemonType::Dragon, PokemonType::Dragon) => 2.0,
-            (PokemonType::Dragon, PokemonType::Steel) => 0.5,
-            (PokemonType::Dragon, PokemonType::Fairy) => 0.0,
-
-            (PokemonType::Dark, PokemonType::Psychic) => 2.0,
-            (PokemonType::Dark, PokemonType::Ghost) => 2.0,
-            (PokemonType::Dark, PokemonType::Fighting) => 0.5,
-            (PokemonType::Dark, PokemonType::Dark) => 0.5,
-            (PokemonType::Dark, PokemonType::Fairy) => 0.5,
-
-            (PokemonType::Steel, PokemonType::Ice) => 2.0,
-            (PokemonType::Steel, PokemonType::Rock) => 2.0,
-            (PokemonType::Steel, PokemonType::Fairy) => 2.0,
-            (PokemonType::Steel, PokemonType::Fire) => 0.5,
-            (PokemonType::Steel, PokemonType::Water) => 0.5,
-            (PokemonType::Steel, PokemonType::Electric) => 0.5,
-            (PokemonType::Steel, PokemonType::Steel) => 0.5,
-
-            (PokemonType::Fairy, PokemonType::Fighting) => 2.0,
-            (PokemonType::Fairy, PokemonType::Dragon) => 2.0,
-            (PokemonType::Fairy, PokemonType::Dark) => 2.0,
-            (PokemonType::Fairy, PokemonType::Fire) => 0.5,
-            (PokemonType::Fairy, PokemonType::Poison) => 0.5,
-            (PokemonType::Fairy, PokemonType::Steel) => 0.5,
-
-            _ => 1.0,
         }
     }
 
@@ -215,7 +71,7 @@ impl BattleEngine {
         for effect in &used_move.effects { //loop over effects
             match effect {
                 MoveEffect::Damage => {
-                    let type_multiplier = Self::get_type_multiplier(&used_move.move_type, &defender.primary_type);
+                    let type_multiplier = get_type_multiplier(&used_move.move_type, &defender.primary_type);
                     let stab_multiplier = if used_move.move_type == attacker.primary_type {
                         1.5
                     } else {
